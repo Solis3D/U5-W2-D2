85d59984c609171ec2f82e20286acf1cc990e54a
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-04-14 18:45:57 +0200

    Update NotFoundException.java

--- src/main/java/solis3d/u5w2d2/exceptions/NotFoundException.java
+++ src/main/java/solis3d/u5w2d2/exceptions/NotFoundException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     public NotFoundException(long id) {
-        super("L'elemento ocn id " + id + " non è stato trovato!");
+        super("L'elemento con id " + id + " non è stato trovato!");
     }
 
 }
